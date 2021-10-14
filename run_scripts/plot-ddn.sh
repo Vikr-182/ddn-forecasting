9001ff5c211ee382cda856b4a5954c32be8090fe
Vikr-182 <vikrant.dewangan@research.iiit.ac.in>
2021-10-14 21:19:23 +0530

    changed plot_traj

--- run_scripts/plot-ddn.sh
+++ run_scripts/plot-ddn.sh
@@ -3,7 +3,8 @@
 #SBATCH --time=1-72:00:00
 #SBATCH --mincpus=5
 #SBATCH --output /home2/vikrant.dewangan/_out/2_train_ddn.log
-#SBATCH -G 2 -c 20
+#SBATCH -G 3 -c 20
+#SBATCH -w gnode33
 #SBATCH --mail-type=ALL --mail-user=vikrant.dewangan@research.iiit.ac.in
 
 #module load cuda/11.0
@@ -13,14 +14,14 @@ DATASET_DST="/scratch/forecasting"
 
 mkdir -p $DATASET_DST
 cd $DATASET_DST
-scp ada:/share1/vikrant.dewangan/dataset/argoverse/forecasting_train_v1.1.tar.gz   ./
-scp ada:/share1/vikrant.dewangan/dataset/argoverse/forecasting_val_v1.1.tar.gz ./
+#scp ada:/share1/vikrant.dewangan/dataset/argoverse/forecasting_train_v1.1.tar.gz   ./
+#scp ada:/share1/vikrant.dewangan/dataset/argoverse/forecasting_val_v1.1.tar.gz ./
 
 #wget https://s3.amazonaws.com/argoai-argoverse/forecasting_train_v1.1.tar.gz
 #wget https://s3.amazonaws.com/argoai-argoverse/forecasting_val_v1.1.tar.gz
 
-tar -zxvf forecasting_train_v1.1.tar.gz
-tar -zxvf forecasting_val_v1.1.tar.gz
+#tar -zxf forecasting_train_v1.1.tar.gz
+#tar -zxvf forecasting_val_v1.1.tar.gz
 
 echo "Done copying"
 #conda activate forecasting
@@ -28,5 +29,5 @@ echo "Done conda"
 echo "LALA" >> lala.txt
 #cd ~/forecasting/sengming/argo-forecasting-competition
 cd ~/forecasting/ddn-forecasting
-python3 script_train.py --train_dir /scratch/forecasting/train/data/ --test True --test_dir /scratch/forecasting/val/data/ --network LSTMPredHeading --lr 0.0005 --end_epoch 50  --train_batch_size 2048 --num_workers 20
+python3 plot_traj.py --train_dir /scratch/forecasting/train/data/ --test True --test_dir /scratch/forecasting/val/data/ --network LSTMPredHeading --lr 0.0001 --end_epoch 50  --train_batch_size 20 --num_workers 20
 echo "Done Training"
