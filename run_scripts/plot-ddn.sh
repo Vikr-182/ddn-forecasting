619498f4eca61691c8009d82baeac470e73bd90d
Vikr-182 <vikrant.dewangan@research.iiit.ac.in>
2021-10-14 22:08:57 +0530

    added scripts for plot

--- run_scripts/plot-ddn.sh
+++ run_scripts/plot-ddn.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=1-72:00:00
 #SBATCH --mincpus=5
 #SBATCH --output /home2/vikrant.dewangan/_out/2_train_ddn.log
-#SBATCH -G 3 -c 20
+#SBATCH -G 0 -c 5
 #SBATCH -w gnode33
 #SBATCH --mail-type=ALL --mail-user=vikrant.dewangan@research.iiit.ac.in
 
@@ -14,7 +14,7 @@ DATASET_DST="/scratch/forecasting"
 
 mkdir -p $DATASET_DST
 cd $DATASET_DST
-#scp ada:/share1/vikrant.dewangan/dataset/argoverse/forecasting_train_v1.1.tar.gz   ./
+scp ada:/share1/vikrant.dewangan/dataset/argoverse/val_data.npy   ./
 #scp ada:/share1/vikrant.dewangan/dataset/argoverse/forecasting_val_v1.1.tar.gz ./
 
 #wget https://s3.amazonaws.com/argoai-argoverse/forecasting_train_v1.1.tar.gz
@@ -29,5 +29,5 @@ echo "Done conda"
 echo "LALA" >> lala.txt
 #cd ~/forecasting/sengming/argo-forecasting-competition
 cd ~/forecasting/ddn-forecasting
-python3 plot_traj.py --train_dir /scratch/forecasting/train/data/ --test True --test_dir /scratch/forecasting/val/data/ --network LSTMPredHeading --lr 0.0001 --end_epoch 50  --train_batch_size 20 --num_workers 20
+python3 plot_traj.py --test_dir /scratch/forecasting/val_data.npy --network LSTMEP --model_path  checkpoints/final_with.ckpt --test True
 echo "Done Training"
